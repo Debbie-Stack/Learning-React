3401f7e4de72472dd836ce57f2e9ab1cae0bda68
Debbie-Stack <deboraheffiong202@gmail.com>
2026-03-16 15:12:30 +0100

    learning react

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+const HomePage = ()=>{
+  return (
+    <div>
+      <h1 className="text-4xl bg-amber-500">Deborah</h1>
+      <NavBar />
+    </div>
+  )
+}
+
+export default HomePage;
