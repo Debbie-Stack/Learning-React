8bcd1e9630a69983cc21a15c71dc9645fcca1e15
Debbie-Stack <deboraheffiong202@gmail.com>
2026-03-21 22:43:10 +0100

    created a students exam report

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import user1 from "./assets/carolynbeekhuis-portrait-photo-3001326.jpg"
 import user2 from "./assets/meandyou2-headshot-2644201_640.jpg"
 import user3 from "./assets/7000920-baby-2953791_640.jpg"
 import user4 from "./assets/gart-self-portrait-687589.jpg"
-import user5 from "./assets/meandyou2-headshot-2644201_640.jpg"
 
 const App = () => {
 
   return (
-    <div> 
-      <Welcome name = "John" age="21" grade="Excellent (A)" image={user1}/>
-      <Welcome name = "Mike" age="26" grade="Good (C)" image={user2}/>
-      <Welcome name = "Faith" age="28" grade="Pass (D)" image={user3}/>
-      <Welcome name = "Joy" age="25" grade="Fair (E)" image={user4}/>
-      <Welcome name = "Peter" age="32" grade="Failed (F)" image={user5}/>
+    <div className="bg-indigo-300 h-[105vh]">
+      <h1 className="ml-130 font-bold text-xl mb-7 text-white border-b-5">Exams Report for Grade 1 Pupils</h1> 
+      <Welcome name = "John Adams" age="6" gender="Male" grade="Excellent (A)" image={user1}/>
+      <Welcome name = "Mike Jordan" age="7" gender="Male" grade="Good (C)" image={user2}/>
+      <Welcome name = "Faith Ado" age="6" gender="Female" grade="Pass (D)" image={user3}/>
+      <Welcome name = "Joy Yaxs" age="7" gender="Female" grade="Fair (E)" image={user4}/>
     </div>
   )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,17 +1,18 @@
 
-const Welcome = ({name, age, grade, image
+const Welcome = ({name, age, gender, grade, image
 
 }) => {
   return (
-    <div className="flex justify-center gap-10 ">
-      <div>
+    <div className="flex justify-center items-center gap-10 mb-6">
+      <div className="border-2 rounded-full border-violet-500">
       <img src={image} alt="user image" className="w-30 h-30 rounded-full" />
       </div>
 
-      <div>
-      <h1>Name: {name}</h1>
-      <h1>Age: {age}</h1>
-      <h1>Grade: {grade}</h1>
+      <div className="text-white font-bold text-lg">
+      <h1 className="border-t-3 border-b-3 p-1 border-dashed">Name : {name}</h1>
+      <h1>Age : {age}</h1>
+      <h1>Gender : {gender}</h1>
+      <h1>Grade : {grade}</h1>
       </div>
     </div>
 
